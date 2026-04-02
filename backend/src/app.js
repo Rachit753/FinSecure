c59c58660008e3422eeee5691c7cc38f7a007883
Rachit753 <chauhanrachit02@gmail.com>
2026-04-02 18:33:52 +0530

    Add JWT authentication middleware to protect routes and verify users

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,23 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import { protect } from "./middleware/authMiddleware.js";
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "You accessed a protected route",
+    user: req.user,
+  });
 });
 
 export default app;
